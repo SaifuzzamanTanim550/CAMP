ca80285794c3ebb0b00190401f1b99efee5e68c0
ColvinKopram <154466436+ColvinKopram@users.noreply.github.com>
2025-11-29 01:48:12 +0000

    changes to landingpage visuals

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -18,7 +18,7 @@ export default function LandingPage() {
     return (
         <div className="w-screen h-screen flex flex-col bg-black dark:bg-white relative">
             <div className='w-full flex justify-center mt-8'>
-                <h1 className="relative xl:text-9xl md:text-8xl text-5xl sm:tracking-[17px] tracking-[10px] uppercase text-center leading-[0.70em] outline-none animate-dimlight box-reflect">
+                <h1 className="relative xl:text-9xl md:text-8xl text-5xl sm:tracking-[17px] tracking-[10px] uppercase text-center leading-[0.70em] outline-none animate-police box-reflect text-white dark:text-black">
                     WELCOME TO C.A.M.P
                 </h1>
             </div>
@@ -35,7 +35,7 @@ export default function LandingPage() {
                         className='rounded-md border-2 border-black w-[180px] h-[60px] transition-all hover:scale-125 hover:animate-pulseGlow hover:text-xl'
                         onClick={() => navigate(path)}
                     >
-                        <div className='bg-sample w-full h-full flex items-center justify-center'>
+                        <div className='bg-sample w-full h-full flex items-center justify-center text-white dark:text-black'>
                             {label}
                         </div>
                     </button>
@@ -45,10 +45,12 @@ export default function LandingPage() {
             
             <div className="absolute bottom-6 right-6">
                 <button 
-                    className='px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600' 
-                    onClick={() => setDark(!dark)}>
-                    Dark Mode Toggle
-
+                    onClick={() => setDark(!dark)}
+                    className='rounded-md border-2 border-black w-[140px] h-[50px] transition-all hover:scale-110 hover:animate-pulseGlow hover:text-xl'
+                >
+                    <div className='w-full h-full flex items-center justify-center'>
+                        {dark ? "Light Mode" : "Dark Mode"}
+                    </div>
                 </button>
             </div>
         </div>
